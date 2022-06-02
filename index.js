b0b9944c6be8dac79769446cef53475f53f1da7f
github username <github email address>
2022-06-02 17:24:15 -0400

    commit tree 32d4fe2a42dbec16a7c813c5f060fc708013e8ca

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array,name){
+  return  array.filter(driver=>driver.toUpperCase()===name.toUpperCase())
+}
+
+function fuzzyMatch(array,string){
+  return  array.filter(x=>x.startsWith(string))
+}
+
+function matchName(object,name){
+   return object.filter(x=>x.name===name)
+}
